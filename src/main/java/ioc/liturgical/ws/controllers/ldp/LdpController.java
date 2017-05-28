275d750d6b9fae5fcc59f41a975d70d9dcef4951
pblattnerjr <blattner@aracnet.com>
2017-05-27 17:45:47 -0700

    Modifications to LdpController made in Bend

--- src/main/java/ioc/liturgical/ws/controllers/ldp/LdpController.java
+++ src/main/java/ioc/liturgical/ws/controllers/ldp/LdpController.java
@@ -2,14 +2,22 @@ package ioc.liturgical.ws.controllers.ldp;
 
 import static spark.Spark.get;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import ioc.liturgical.ws.constants.ADMIN_ENDPOINTS;
 import ioc.liturgical.ws.constants.Constants;
+import ioc.liturgical.ws.controllers.admin.ControllerUtils;
+import ioc.liturgical.ws.controllers.admin.LabelsController;
 import ioc.liturgical.ws.managers.ldp.LdpManager;
 import ioc.liturgical.ws.models.ResultJsonObjectArray;
 
 public class LdpController {
-	
+	private static final Logger logger = LoggerFactory.getLogger(LdpController.class);
+
     private Gson gson = new GsonBuilder().disableHtmlEscaping().create();
     
 	/**
@@ -19,18 +27,20 @@ public class LdpController {
 	 * @param manager
 	 */
 	public LdpController(LdpManager manager) {
-		
-		get(Constants.INTERNAL_LITURGICAL_DAY_PROPERTIES_API_PATH + "/ldp" , (request, response) -> {
+		String path = Constants.INTERNAL_LITURGICAL_DAY_PROPERTIES_API_PATH + "/ldp/";
+		ControllerUtils.reportPath(logger, "GET", path);
+
+		get(path , (request, response) -> {
 			response.type(Constants.UTF_JSON);
 			String calendarType = request.queryParams("t"); // calendar types - see LITURGICAL_CALENDAR_TYPE enum for values
-			String date = request.queryParams("d"); // date ISO String, ex: "2016-11-19T12:00:00.000Z" 
+			String date         = request.queryParams("d"); // date ISO String, ex: "2016-11-19T12:00:00.000Z" 
 			boolean noType = (calendarType == null || calendarType.length() < 0);
 			boolean noDate = (date == null || date.length() < 0);
 			ResultJsonObjectArray result = null;
 			if (noType && noDate) {
 				result = manager.getLdpForToday();
 			} else {
-				if (noType) {
+				if (noDate) {
 		        	result = manager.getLdpForToday(calendarType);
 				} else {
 		        	result = manager.getLdpForDate(
